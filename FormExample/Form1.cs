650a998ae85db8bb96ee2f11d6385fbb6555ca53
Philip Plouch <Philip Plouch>
2017-02-19 21:18:15 -0600

    Initial Commit

---
+++ FormExample/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace FormExample
+{
+    public partial class Form1 : Form
+    {
+        public static Form form;
+        public static Thread thread;
+
+        public static Dovahkiin hero = new Dovahkiin();
+        public static int s = 100;
+        public static int fps = 30;
+        public static double running_fps = 30.0;
+        public static Font font = new Font("Ubuntu", 12);
+        public static int counter = 1;
+        
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            hero.Image = Properties.Resources.Dov;
+            DoubleBuffered = true;
+            form = this;
+            thread = new Thread(new ThreadStart(Update));
+            thread.Start();
+
+        }
+
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            thread.Abort();
+        }
+        
+        
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            hero.act();
+            hero.render(e.Graphics);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static void Update()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while(true)
+            {
+                DateTime temp = DateTime.Now;
+                running_fps = .9 * running_fps + .1 * 1000.0 / (temp - now).TotalMilliseconds;
+                Console.WriteLine(running_fps);
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime - diff).Milliseconds);
+                last = DateTime.Now;
+                form.Invoke(new MethodInvoker(form.Refresh));
+            }
+        }
+    }
+    
+}
---
+++ FormExample/Sprite.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+public class Sprite
+{
+	private Sprite parent = null;
+
+	//instance variable
+	private float x=0;
+
+	public float X
+	{
+		get { return x; }
+		set { x = value; }
+	}
+
+	private float y = 0;
+
+	public float Y
+	{
+		get { return y; }
+		set { y = value; }
+	}
+
+	private float scale = 1;
+
+	public float Scale
+	{
+		get { return scale; }
+		set { scale = value; }
+	}
+
+	private float rotation =0;
+
+	/// <summary>
+	/// The rotation in degrees.
+	/// </summary>
+	public float Rotation
+	{
+		get { return rotation; }
+		set { rotation = value; }
+	}
+
+
+	public List<Sprite> children = new List<Sprite>();
+
+
+	public void Kill()
+	{
+		parent.children.Remove(this);
+	}
+
+	//methods
+	public void render(Graphics g)
+	{
+		Matrix original = g.Transform.Clone();
+		g.TranslateTransform(x, y);
+		g.ScaleTransform(scale, scale);
+		g.RotateTransform(rotation);
+		paint(g);
+		foreach(Sprite s in children)
+		{
+			s.render(g);
+		}
+		g.Transform = original;
+	}
+
+	public virtual void paint(Graphics g)
+	{
+
+	}
+
+	public virtual void act()
+	{
+
+	}
+
+	public void add(Sprite s)
+	{
+		s.parent = this;
+		children.Add(s);
+	}
+
+
+
+}
